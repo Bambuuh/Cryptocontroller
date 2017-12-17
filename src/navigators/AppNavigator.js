0fb6ef76769b386dabe4c40449edad14668f5abc
Lukas Klingberg <lukas.klingberg@redmind.se>
2017-12-17 23:45:55 +0100

    better tabs

--- src/navigators/AppNavigator.js
+++ src/navigators/AppNavigator.js
@@ -1,33 +1,51 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import { addNavigationHelpers, StackNavigator, TabNavigator } from 'react-navigation'
+import { Text } from 'react-native'
+import { addNavigationHelpers, StackNavigator, TabNavigator , TabBarTop} from 'react-navigation'
 
 import CurrenciesMonth from '../components/scenes/currencies/CurrenciesMonth'
 import CurrenciesWeek from '../components/scenes/currencies/CurrenciesWeek'
 import CurrencyDetails from '../components/scenes/CurrencyDetails'
 
+const labelStyle = (props, alignSelf, marginVertical) => ({
+    fontSize: 13,
+    fontWeight: '600',
+    marginHorizontal: 15,
+    marginVertical,
+    color: props.focused ? props.tintColor : "#929292",
+    alignSelf
+});
+// IndicatorStyle is an absolute positioned View
+const indicatorStyle = (props, alignSelf) => ({
+    backgroundColor: props.activeTintColor,
+    alignSelf: 'flex-end',
+});
+
 const HomeScreenNavigator = TabNavigator(
     {
-        Week: { screen: CurrenciesWeek },
-        Month: { screen: CurrenciesMonth }
+        Week: {
+            screen: CurrenciesWeek,
+            navigationOptions: {
+                tabBarLabel: (props) => (<Text style={labelStyle(props, 'flex-end', 10)}> WEEK </Text>)
+            },
+        },
+        Month: {
+            screen: CurrenciesMonth,
+            navigationOptions: {
+                tabBarLabel: (props) => (<Text style={labelStyle(props, 'flex-start', 10)}> MONTH </Text>)
+            }
+        }
     }, {
+        tabBarComponent: (props)=> <TabBarTop {...props} indicatorStyle={indicatorStyle(props, 'flex-end')} />,
         initialRouteName: 'Week',
         tabBarPosition: 'top',
         tabBarOptions: {
             activeTintColor: '#2980b9',  // Color of tab when pressed
             inactiveTintColor: '#bdc3c7', // Color of tab when not pressed
-            labelStyle: {
-                fontSize: 20,
-            },
             style: {
                 backgroundColor: '#fff', // Makes Android tab bar white instead of standard blue
-                height: 30
             },
-            indicatorStyle: {
-                borderBottomColor: '#2980b9',
-                borderBottomWidth: 2,
-            }
         },
     }
 )
